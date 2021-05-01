31c81c0555db2f0e884558bdf85d5d3eb7cdc493
Helder Pinhal <helder@notifica.re>
2021-05-01 19:31:37 +0100

    chore: update push example

--- notificare_push/example/android/app/src/main/kotlin/re/notifica/push/notificare_push_example/MainActivity.kt
+++ notificare_push/example/android/app/src/main/kotlin/re/notifica/push/notificare_push_example/MainActivity.kt
@@ -2,14 +2,13 @@ package re.notifica.push.notificare_push_example
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import io.flutter.embedding.android.FlutterActivity
 import re.notifica.push.NotificarePush
 
 class MainActivity: FlutterActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         NotificarePush.handleTrampolineIntent(intent)
     }
 
