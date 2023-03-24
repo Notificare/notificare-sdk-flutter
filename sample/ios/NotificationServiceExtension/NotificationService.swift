1fc0e8b64308ecd384e0ab0bb8cf0eeec43be8c5
Helder Pinhal <helder@notifica.re>
2023-03-24 11:31:15 +0000

    feat(sample): add notification service extension

--- sample/ios/NotificationServiceExtension/NotificationService.swift
+++ sample/ios/NotificationServiceExtension/NotificationService.swift
@@ -6,30 +6,26 @@
 //
 
 import UserNotifications
+import NotificareNotificationServiceExtensionKit
 
 class NotificationService: UNNotificationServiceExtension {
 
-    var contentHandler: ((UNNotificationContent) -> Void)?
-    var bestAttemptContent: UNMutableNotificationContent?
-
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
-        self.contentHandler = contentHandler
-        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        
-        if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
-            contentHandler(bestAttemptContent)
+        NotificareNotificationServiceExtension.handleNotificationRequest(request) { result in
+            switch result {
+            case let .success(content):
+                contentHandler(content)
+
+            case let .failure(error):
+                print("Failed to handle the notification request.\n\(error)")
+                contentHandler(request.content)
+            }
         }
     }
     
     override func serviceExtensionTimeWillExpire() {
         // Called just before the extension will be terminated by the system.
         // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-            contentHandler(bestAttemptContent)
-        }
     }
 
 }
